37c0c5c0fee4fb0efc63025be47cee6acbcdc68f
Hasin-Al <csebrur.hasinmanjare34@gmail.com>
2025-09-01 14:30:52 +0600

    check dir exists or not and add merged.txt as training and val file

--- TrainTokenizer.py
+++ TrainTokenizer.py
@@ -1,6 +1,5 @@
-<<<<<<< HEAD
+import os
 from utils import read_text_file, add_eos, train_and_save_bpe_tokenizer
-
 
 vocab_size = 10000
 
@@ -8,17 +7,8 @@ text_filepath = '/merged.txt'
 text = read_text_file(text_filepath)
 text_with_eos = add_eos(text)
 
-output_dir = "/tokenizer/bpe_tokenizer.json" # change this directory link
-=======
-from utils import read_text_file, add_eos, train_and_save_bpe_tokenizer
+# Ensure the directory exists
+output_dir = "/tokenizer/bpe_tokenizer.json"
+os.makedirs(os.path.dirname(output_dir), exist_ok=True)
 
-
-vocab_size = 10000
-
-text_filepath = '/merged.txt'
-text = read_text_file(text_filepath)
-text_with_eos = add_eos(text)
-
-output_dir = "/tokenizer/bpe_tokenizer.json" # change this directory link
->>>>>>> b2a3c63f3e93c5bb09be358e3702f1e3bcc310d1
-sentences= train_and_save_bpe_tokenizer(text_with_eos,vocab_size,output_dir)
+sentences = train_and_save_bpe_tokenizer(text_with_eos, vocab_size, output_dir)
